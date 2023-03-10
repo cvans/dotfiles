b77e24e3497b0ce2f9f511dd99923692aab93484
cvans <icharles@hey.com>
2023-03-09 19:13:42 -0600

    Add boostrap script for windows installations.

---
+++ bin/bootstrap-win.ps1
@@ -0,0 +1,65 @@
+# This sciprt assumes that winget is already installed.
+
+# Define array of applications to install using winget
+$applications = @(
+    "7zip.7zip",
+    "Anki.Anki",
+    "AgileBits.1Password",
+    "Discord.Discord",
+    "FastStone.Capture",
+    "Git.Git",
+    "Microsoft.PowerToys",
+    "Microsoft.VisualStudioCode",
+    "NeoVim.Neovim",
+    "PeterPawlowski.foobar2000",
+    "qBittorrent.qBittorrent",
+    "Spotify.Spotify",
+    "Starship.Starship",
+    "Valve.Steam",
+    "WireGuard.WireGuard" 
+)
+
+# Define array of buckets to add using scoop
+$scoopbuckets = @(
+    "extras",
+    "nerd-fonts"
+)
+
+# Define array of applications to install using scoop
+$scoopapps = @(
+    "mpv",
+    "terminal-icons",
+    "Cascadia-Code",
+    "FiraCode",
+    "JetBrains-Mono"
+)
+
+# Install applications using winget
+foreach ($app in $applications) {
+    Write-Host "[INFO] Starting to install $app..." 
+    winget install $app -e
+    Write-Host "[INFO] Finished installing $app..." 
+}
+
+# Reload PowerShell session
+powershell.exe -NoProfile -Command "& {[System.Environment]::Exit(0)}"
+
+# Install Scoop
+Write-Host "[INFO] Starting to install scoop..."
+iwr -useb get.scoop.sh | iex
+Write-Host "[INFO] Finished installing scoop..."
+
+# Add scoop buckets
+foreach ($bucket in $scoopbuckets) {
+    Write-Host "[INFO] Starting to add bucket $app..." 
+    scoop bucket add $bucket
+    Write-Host "[INFO] Finished adding bucket $app..." 
+}
+
+# Add scoop apps
+foreach ($app in $scoopapps) {
+    Write-Host "[INFO] Starting to install $app..." 
+    scoop install add $bucket
+    Write-Host "[INFO] Finished installing $app..." 
+}
+
