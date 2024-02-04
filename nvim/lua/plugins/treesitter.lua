e6049351bd5f812b04c88219903b6bb25ffbfa20
cvans <icharles@hey.com>
2024-02-03 22:36:06 -0600

    Add lualine, neotree

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "c_sharp", "css", "html", "javascript", "lua", "python", "ruby", "rust", "vim" }, 
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
