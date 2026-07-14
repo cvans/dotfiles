6fa25d44a62f741f851fa816e78ee8075e476a54
cvans <icharles@hey.com>
2026-07-13 21:16:54 -0600

    catppuccin theme update

--- dot_local/share/theme-library/catppuccin/neovim.lua
+++ dot_local/share/theme-library/catppuccin/neovim.lua
@@ -3,11 +3,14 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
+		opts = {
+			transparent_background = true,
+		},
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "catppuccin-nvim",
 		},
 	},
 }
